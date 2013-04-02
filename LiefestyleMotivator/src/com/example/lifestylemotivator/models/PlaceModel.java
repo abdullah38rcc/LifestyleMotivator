fe65aca4867429657e0fa0362a6b21e73306567f
Vadim <vnikiti@ncsu.edu>
2013-04-02 01:13:40 -0400

    Model that represents a place.

---
+++ LiefestyleMotivator/src/com/example/lifestylemotivator/models/PlaceModel.java
@@ -0,0 +1,35 @@
+package com.example.lifestylemotivator.models;
+
+public class PlaceModel {
+
+private String name;
+private String[] type;
+private String latitude;
+private String longitude;
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String[] getType() {
+	return type;
+}
+public void setType(String[] type) {
+	this.type = type;
+}
+public String getLatitude() {
+	return latitude;
+}
+public void setLatitude(String latitude) {
+	this.latitude = latitude;
+}
+public String getLongitude() {
+	return longitude;
+}
+public void setLongitude(String longitude) {
+	this.longitude = longitude;
+}
+
+}
